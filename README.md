2a55b8a8a3e1533d79f8295535a904ce7abf9ab0
Alexander <alexanderbeloiu@gmail.com>
2023-02-03 17:58:19 -0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # slugNotes
 Cruzhacks 2023
+
+#Participants
+Alexander Beloiu
+Kelly lin
+Jeffrey
+Aidan
---
+++ main.py
@@ -0,0 +1,12 @@
+#initialize a flask app for hello world
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+
+if __name__ == "__main__":
+    app.run()
+    
