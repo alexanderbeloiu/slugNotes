dcb74e50de9da382ab9ad4c3b4b08e1fa3786a54
Alexander <alexanderbeloiu@gmail.com>
2023-02-03 23:05:26 -0800

    alex is going to bed

--- database.py
+++ database.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 Base = declarative_base()
 
+#this configures all the fields that are in the database
 class Ufile(Base):
     __tablename__ = 'files'
     id = Column(Integer, primary_key=True)
@@ -16,11 +17,11 @@ class Ufile(Base):
     user_id = Column(Integer)
     path = Column(String)
 
-
+#the main class you will be interacting with
 class Files():
     def __init__(self):
         # Connect to the database
-        engine = create_engine('sqlite:///example2.db')
+        engine = create_engine('sqlite:///files.db')
 
         # Create the tables
         Base.metadata.create_all(engine)
@@ -29,23 +30,29 @@ class Files():
         Session = sessionmaker(bind=engine)
         self.session = Session()
 
+    #adds a file to the database. It takes the file info as arguments, and puts them as columns in the database
     def add_file(self, name, class_name, week_name, file_name, size, date, type, user_id, path):
         self.session.add(Ufile(name=name, class_name=class_name, week_name=week_name, file_name=file_name, size=size, date=date, type=type, user_id=user_id, path=path))
         self.session.commit()
 
+    #gets the gives you the file path when you give it the class name, week name, and note name
     def get_file_path(self,class_name,week_name,file_name):
         file = self.session.query(Ufile).filter_by(class_name=class_name, week_name=week_name, file_name=file_name).first()
         return file.path
+   
     #this function looks in the database, and get a list of all the classes
     def get_classes_list(self):
         classes = self.session.query(Ufile.class_name).distinct().all()
         classes = [i[0] for i in classes]
         return classes
+    
+    #this function looks in the database, and get a list of all the weeks in the specified class
     def get_weeks_list(self, class_name):
         weeks = self.session.query(Ufile.week_name).filter_by(class_name=class_name).distinct().all()
         weeks = [i[0] for i in weeks]
         return weeks
 
+    #this function looks in the database, and get a list of all the files in the specified class and week
     def get_files_list(self, class_name, week_name):
         files = self.session.query(Ufile.file_name).filter_by(class_name=class_name, week_name=week_name).all()
         files = [i[0] for i in files]
@@ -57,6 +64,7 @@ class Files():
 
 
 if __name__ == '__main__':
+    #example of how you can use it
     f = Files() 
     f.add_file('hw1', 'cs61a', 'week1', 'hw1.py', 100, '2019-01-01', 'python', 1, '/home/cs61a/hw1.py')
     print(f.get_file_path('cs61a', 'week1', 'hw1.py'))
--- main.py
+++ main.py
@@ -1,11 +1,75 @@
-#initialize a flask app for hello world
-from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+import os
+from flask import Flask, flash, request, redirect, url_for, render_template, request, redirect, url_for, flash, jsonify
+from werkzeug.utils import secure_filename
+import hashlib
+import uuid
+import database
+from datetime import datetime
+UPLOAD_FOLDER = 'data'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif','c','py','docx','doc'}
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = "ouhojaheoduh[a0uhfouahd;oubasdoiholjawbe;h[oiabfljhvipuh"
+
+#this is the object that interacts with the database
+file_info=database.Files()
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+#this function saves the file to the disk, then adds it to the database
+def save_file(file):
+    #secure filename does something to prevent hacking
+    filename = secure_filename(file.filename)
+    
+    #adds the file to the database
+    file_info.add_file(filename,'CSE_30','week1',filename,100,datetime.today().strftime('%Y-%m-%d %H:%M:%S'),filename[filename.rindex("."):len(filename)],1,UPLOAD_FOLDER+'/'+filename)
+    
+    #saves the file to the disk
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+    return filename
+
+#how to do file upload https://flask.palletsprojects.com/en/2.2.x/patterns/fileuploads/
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            
+            filename=save_file(file)
+            return redirect(url_for('upload_file', name=filename))
+    
+    
+    #you guys can try to convert to the below code to a template
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
 
 @app.route("/")
 def hello():
     return render_template('index.html')
 
 
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(port=4000, debug=True)  
